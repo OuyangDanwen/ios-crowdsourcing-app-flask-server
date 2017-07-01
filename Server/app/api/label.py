5602e5a4a014d95177e261cd90befe2eb3bb43ff
Muhammad Bhatti <mdr.bhatti@tum.de>
2017-07-01 02:36:05 +0200

    refactor: move label routes to file

---
+++ Server/app/api/label.py
@@ -0,0 +1,59 @@
+from . import *
+
+
+@app.route('/api/labels', methods=['GET'])
+def getLabels():
+    lbs = [lb for lb in Label.objects()]
+    labels_dict_list = json.loads(json.dumps(lbs, cls=MongoEncoder))
+    labels_list = []
+    for lb in labels_dict_list:
+        lb["images"] = Image.objects(label=lb["name"]).count()
+        lb["resources"] = Resource.objects(label=lb["name"]).count()
+        labels_list.append(lb)
+    ret = {
+        "labels": labels_list
+    }
+    return jsonify(ret), 200
+
+@app.route('/api/labels', methods = ['POST'])
+@jwt_required
+def postLabel():
+    label = request.form["label"].lower()
+    if not len(label) > 0:
+        return jsonify({'msg': 'No label found'}), 400
+    username = get_jwt_identity_override()
+    # Save label
+    saveLabel(label, username)
+    files = request.files.getlist('files[]')
+    saveLabelPhotos(files, label, username)
+    return jsonify({'msg': 'Done'}), 200
+
+@app.route('/api/labels/', methods = ['PUT'])
+def putLabel():
+    # Modify label and whatever it is referencing
+    old_label = request.json.get('label', None).lower()
+    new_label = request.json.get('newlabel', None).lower()
+    if not len(old_label) > 0:
+        return jsonify({'msg': 'No label found in request'}), 403
+    labelList = Label.objects(name=old_label)
+    if len(labelList) == 0:
+        return jsonify({"msg": "Label doesn't exist!"}), 401
+    new_path = editLabel(old_label, new_label)
+    # Change labelname in database
+    Label.objects(name=old_label).update_one(name=new_label, path=new_path)
+    # Change label in folder
+    # Change label in database (resources
+    Resource.objects(label=old_label).update(label=new_label)
+    return jsonify({'msg': 'Done'}), 200
+
+# TODO: Delete from filesystem (both resources, and everything associated with a label :'( )
+@app.route('/api/labels/<label>', methods = ['DELETE'])
+def deleteLabel(label):
+    if not len(label) > 0:
+        return jsonify({'msg': 'No label found in request'}), 403
+    label_obj = Label.objects(name=label)
+    if len(label_obj) == 0:
+        return jsonify({"msg": "Label doesn't exist!"}), 401
+    # try:
+    Label.objects(name=label).delete()
+    return jsonify({'msg': 'Done'}), 200
