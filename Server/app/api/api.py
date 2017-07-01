ce8c49b6fe0ab1a8fb333002f93b5b4d2041e81c
Muhammad Bhatti <mdr.bhatti@tum.de>
2017-07-01 02:38:27 +0200

    refactor: move image routes to file

--- Server/app/api/api.py
+++ Server/app/api/api.py
@@ -1,7 +1,6 @@
 from . import *
 
 
-
 # Route for testing if auth works
 @app.route('/api/protected', methods=['GET'])
 @jwt_required
@@ -13,7 +12,6 @@ def protected():
 @jwt_required
 def version():
     return jsonify({'version': "1.1.0"}), 200
-
 
 @app.route('/api/retraining_info', methods=['GET'])
 @jwt_required
@@ -29,44 +27,6 @@ def getRetrainingInfo():
 @jwt_required
 def getBackendVersion():
     return jsonify({'version': CURRENT_SERVER_VERSION}), 200
-
-
-
-# Get all images
-@app.route('/api/images', methods=['GET'])
-def getImages():
-    imgs = [img for img in Image.objects().order_by("-createdOn")]
-    ret = {
-        "images": json.loads(json.dumps(imgs, cls=MongoEncoder))
-    }
-    return jsonify(ret), 200
-
-@app.route('/api/images/<label>', methods=['GET'])
-def getImagesLabel(label):
-    imgs = [img for img in Image.objects(label=label).order_by("-createdOn")]
-    ret = {
-        "images": json.loads(json.dumps(imgs, cls=MongoEncoder))
-    }
-    return jsonify(ret), 200
-
-# TODO: Delete image from FS
-@app.route('/api/images/<label>/<name>', methods = ['DELETE'])
-def deleteImage(label, name):
-    path = "/home/ec2-user/Server/file_system/train_image/" + label + "/" + name
-    if not len(path) > 0:
-        return jsonify({'msg': 'No Image found in request'}), 403
-    img_obj = Image.objects(path=path)
-    if len(img_obj) == 0:
-        return jsonify({"msg": "Image doesn't exist!"}), 401
-    # try:
-    Image.objects(path=path).delete()
-    return jsonify({'msg': 'Done'}), 200
-
-# Check if path is valid!!!
-@app.route('/api/images/<label>/<name>', methods=['GET'])
-def sendImage(label, name):
-    base = "/home/ec2-user/Server/file_system/train_image/" + label + "/" + name
-    return send_file(base, mimetype='image/jpeg')
 
 @app.route('/api/mindsight/predictions/validate', methods=['POST'])
 @jwt_required
