09a871c81dba7f5e68ec445190fa7f214d00cab5
Muhammad Bhatti <mdr.bhatti@tum.de>
2017-07-02 01:47:08 +0200

    feat: add ContentFeedAdapter

---
+++ Server/app/adapter/ContentFeedAdapter.py
@@ -0,0 +1,12 @@
+class ContentFeedAdapter:
+    def __init__(self, query, max_results, location):
+        self.query = query
+        self.max_results = max_results
+        self.location = location
+
+
+class GoogleContentFeedAdapter(ContentFeedAdapter):
+    pass
+
+if __name__ == '__main__':
+    print "working"
