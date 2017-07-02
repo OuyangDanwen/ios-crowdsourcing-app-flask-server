f02d34855a96ca7f86ad787c644556f7a3d53fa6
Muhammad Bhatti <mdr.bhatti@tum.de>
2017-07-03 00:31:06 +0200

    refactor: remove unused stylesheet

--- Dashboard/src/app/pages/image-crud/view-photos-modal/view-photos-modal.component.ts
+++ Dashboard/src/app/pages/image-crud/view-photos-modal/view-photos-modal.component.ts
@@ -5,7 +5,6 @@ import { ImageCrudService } from '../image-crud.service';
 @Component({
   selector: 'app-view-photos-modal',
   templateUrl: './view-photos-modal.component.html',
-  styleUrls: ['./view-photos-modal.component.scss']
 })
 
 export class ViewPhotosModalComponent implements OnInit {
