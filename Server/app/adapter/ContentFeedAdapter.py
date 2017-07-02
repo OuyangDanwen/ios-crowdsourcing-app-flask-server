67d85b15a3b4eb81a50c53e2280bd623c6f6d226
Muhammad Bhatti <mdr.bhatti@tum.de>
2017-07-02 02:07:35 +0200

    feat: add google cfa

--- Server/app/adapter/ContentFeedAdapter.py
+++ Server/app/adapter/ContentFeedAdapter.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class ContentFeedAdapter:
     def __init__(self, query, max_results, location):
         self.query = query
@@ -6,7 +9,23 @@ class ContentFeedAdapter:
 
 
 class GoogleContentFeedAdapter(ContentFeedAdapter):
-    pass
+    url = "https://www.googleapis.com/customsearch/v1?key=AIzaSyB85z_G1Uml_RjlV0VpHek-88WslFgd2tE&cx=011709361973084828043:lu0egzudwnk"
 
-if __name__ == '__main__':
-    print "working"
+    def construct_divs(self, item):
+        title = '<h3>{0}</h3>'.format(item["title"].encode('utf-8'))
+        url = '<a href="{0}">{1}</a>'.format(item["formattedUrl"].encode('utf-8'), title)
+        snippet = '<p>{0}</p>'.format(item["htmlSnippet"].encode('utf-8'))
+        div = "<div>{0}{1}</div>".format(url, snippet)
+        return div
+
+    def send_request(self):
+        r = requests.get("{0}&num={1}&q={2}".format(self.url, self.max_results, self.query))
+        return r.json()
+
+    def render_html(self):
+        data = self.send_request()
+        divs = []
+        for item in data["items"]:
+            div = self.construct_divs(item)
+            divs.append(div)
+        return divs
