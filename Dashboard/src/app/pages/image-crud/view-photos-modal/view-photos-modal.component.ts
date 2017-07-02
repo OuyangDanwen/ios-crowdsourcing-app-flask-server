6c2f21062e5b87627583ba8517d7c0c15eba7fce
Muhammad Bhatti <mdr.bhatti@tum.de>
2017-07-03 00:30:17 +0200

    style: fix formatting

--- Dashboard/src/app/pages/image-crud/view-photos-modal/view-photos-modal.component.ts
+++ Dashboard/src/app/pages/image-crud/view-photos-modal/view-photos-modal.component.ts
@@ -1,42 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import {ImageCrudService} from '../image-crud.service';
+import { ImageCrudService } from '../image-crud.service';
+
 @Component({
   selector: 'app-view-photos-modal',
   templateUrl: './view-photos-modal.component.html',
   styleUrls: ['./view-photos-modal.component.scss']
 })
+
 export class ViewPhotosModalComponent implements OnInit {
+
   isView: boolean = false;
   isDelete: boolean = false;
+  imgSrc: string = '';
 
-  imgSrc: string = "";
-    constructor(private activeModal: NgbActiveModal, private imgService: ImageCrudService) {  }
+  constructor(private activeModal: NgbActiveModal, private imgService: ImageCrudService) { }
 
   ngOnInit() {
   }
+
   closeModal() {
     this.activeModal.close();
   }
+
   onModalLaunch(imgSrc, deld) {
     this.imgSrc = imgSrc;
-    if(deld === "delete"){
+    if (deld === 'delete') {
       this.isDelete = true;
     }
-    else{
+    else {
       this.isView = true;
     }
   }
+
   deleteImage() {
     this.imgService.deleteImage(this.imgSrc)
       .subscribe(
       (response) => {
         console.log(response);
-        console.log("Successfully deleted images");
-        setTimeout(() => {this.closeModal(); }, 2000);
+        console.log('Successfully deleted images');
+        setTimeout(() => { this.closeModal(); }, 2000);
       },
       (error) => { console.log(error); }
       );
-
   }
 }
