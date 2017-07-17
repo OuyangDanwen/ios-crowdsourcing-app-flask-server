ed0bdc364a51eb58735f776aaaebeb9d17977c74
Muhammad Bhatti <mdr.bhatti@tum.de>
2017-07-17 18:01:27 +0200

    fix: broken put route

--- Dashboard/src/app/pages/smartTables/edit-label-modal/edit-label-modal.component.ts
+++ Dashboard/src/app/pages/smartTables/edit-label-modal/edit-label-modal.component.ts
@@ -10,6 +10,7 @@ export class EditLabelModalComponent implements OnInit {
   oldLabel: string = "";
   newLabel: string = "";
   createdBy: string = "";
+  id: string;
   rowData;
   modalHeader: string;
 
@@ -18,6 +19,7 @@ export class EditLabelModalComponent implements OnInit {
   ngOnInit() { }
 
   onModalLaunch(rowData) {
+    this.id = rowData._id.$oid;
     this.oldLabel = rowData.name;
     this.newLabel = rowData.name;
     this.createdBy = rowData.createdBy;
@@ -25,7 +27,7 @@ export class EditLabelModalComponent implements OnInit {
   }
 
   updateLabel() {
-    this.stService.editLabel(this.oldLabel, this.newLabel)
+    this.stService.editLabel(this.newLabel, this.id)
       .subscribe(
       (response) => {
         console.log(response);
